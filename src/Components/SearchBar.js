675ab8ef6582f06fca5292f7c6e78403e2909f8f
RebeccaCasw-21 <rebeccacaswell21@gmail.com>
2026-03-15 18:16:11 -0500

    Update SearchBar.js

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,1 +1,29 @@
+import React, { useState } from "react";
 
+function SearchBar({ onSearch }) {
+
+  const [query, setQuery] = useState("");
+
+  const handleSearch = () => {
+    onSearch(query);
+  };
+
+  return (
+    <div>
+
+      <input
+        type="text"
+        placeholder="Search for a movie..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+
+      <button onClick={handleSearch}>
+        Search
+      </button>
+
+    </div>
+  );
+}
+
+export default SearchBar;
